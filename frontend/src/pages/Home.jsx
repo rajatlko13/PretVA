720d5ebae2419a2adedd3567243e431f931a1694
rajatlko13 <rajatlko13@gmail.com>
2020-12-20 13:51:31 +0530

    deploying frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,7 +8,7 @@ class Home extends Component {
             <div className="container text-center text-white" style={{textAlign: 'center', fontFamily: 'Goldman', marginTop: '10%'}}>
                 <p style={{ margin: '0', padding: '0', fontSize: '6vw', lineHeight: '1em', textShadow: '2px 2px 7px blue'}}>It's all about</p>
                 <p style={{ margin: '0', padding:'0', fontSize: '6vw', lineHeight: '1em', textShadow: '2px 2px 7px blue'}}>Saving Lives</p>
-                <p className="mt-2">Our mission is to save lives</p>
+                <p className="mt-2">Welcome to Home Page</p>
             </div>
 
          );
